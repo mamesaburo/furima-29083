51d3d9e57c27d7a0f4f185406804fadeeccd656a
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-11-24 21:03:22 +0900

    バリデーションを「with_options presence: true」の使用の記述に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true, uniqueness: { case_sensitive: true }
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers" }
-  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters" }
-  validates :birth, presence: true
+  with_options presence: true do
+    validates :nickname, uniqueness: { case_sensitive: true }
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers" }
+    validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :birth
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" } do
+      validates :last_name
+      validates :first_name
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters" } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
