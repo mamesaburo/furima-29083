17ff492dc313d14ca4892b6dd9790b00a9278149
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-11-29 14:58:05 +0900

    テーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_area
+  belongs_to :user
+
+  with_options presence: true do
+    validates :images
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :shipping_cost_id, numericality: { other_than: 1 }
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :shipping_days_id, numericality: { other_than: 1 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :items, dependent: :destroy
+  
+  with_options presence: true do
+    validates :nickname, uniqueness: { case_sensitive: true }
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers" }
+    validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :birth
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" } do
+      validates :last_name
+      validates :first_name
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters" } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
+end
