2cbcc534c6128429bff1cb647cb747069abe8ad1
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-11-29 17:18:06 +0900

    ルーティングとコントローラーの設定(create)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:images, :name, :description, :price, :condition_id, :shipping_cost_id, :shipping_area_id, :shipping_days_id, :category_id, :user).merge(user_id: current_user.id)
+  end
 end
