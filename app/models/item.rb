2c275bb3b83ccf518da87fe11afabf46fb533bbf
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-12-05 00:50:55 +0900

    可読性が向上の為、バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :description, length: { maximum: 1000 }
-    validates :category_id, numericality: { other_than: 0, message: "Select" }
-    validates :condition_id, numericality: { other_than: 0, message: "Select" }
-    validates :shipping_cost_id, numericality: { other_than: 0, message: "Select" }
-    validates :shipping_area_id, numericality: { other_than: 0, message: "Select" }
-    validates :shipping_days_id, numericality: { other_than: 0, message: "Select" }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+    with_options numericality: { other_than: 0, message: "Select" } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_cost_id
+      validates :shipping_area_id
+      validates :shipping_days_id
+    end
   end
 end
