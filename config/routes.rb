cfd6ea7ef32c5d70b571807583c7d1fa4a7d53aa
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-12-18 22:17:26 +0900

    ビューファイル、CSSを設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-  resources :purchaseditems, only: [:index]
+  resources :purchased_items, only: [:index]
 end
