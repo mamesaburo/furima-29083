c9d1513953898e986338f60e3df3e3f7ef933c16
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-12-20 13:20:45 +0900

    createアクションをコントローラーに定義

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -5,6 +5,18 @@ class PurchasedItemsController < ApplicationController
   end
 
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :shipping_area_id, :city, :address_detail, :building, :phone_number, :purchased_item_id)
   end
   
 end
