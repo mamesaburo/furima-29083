51febaa5bd96f59064cefc8e3faf6616993ea162
mamesaburo <cinnabar.b.m.b.9597@gmail.com>
2020-12-27 13:01:08 +0900

    Payjpを用いたクレジットカード払い機能実装、Herokuへの環境変数設定

--- app/controllers/purchased_items_controller.rb
+++ app/controllers/purchased_items_controller.rb
@@ -1,12 +1,15 @@
 class PurchasedItemsController < ApplicationController
 
   def index
-    @item = Item.find_by(params[:id])
+    @item = Item.find(params[:item_id])
+    @purchased_item_address = PurchasedItemAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchased_item_address = PurchasedItemAddress.new(address_params)
     if @purchased_item_address.valid?
+      pay_item
       @purchased_item_address.save
       redirect_to root_path
     else
@@ -17,6 +20,15 @@ class PurchasedItemsController < ApplicationController
   private
 
   def address_params
-    params.require(:purchased_item_address).permit(:postal_code, :shipping_area_id, :city, :address_detail, :building, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+    params.require(:purchased_item_address).permit(:postal_code, :shipping_area_id, :city, :address_detail, :building, :phone_number, :item_id).merge(user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: address_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchased_item_address.rb
+++ app/models/purchased_item_address.rb
@@ -1,10 +1,11 @@
 class PurchasedItemAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :shipping_area_id, :city, :address_detail, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :shipping_area_id, :city, :address_detail, :building, :phone_number, :user_id, :item_id, :token
 
-  with_options presence: true  do
+  with_options presence: true do
     validates :city
     validates :address_detail
+    validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :shipping_area_id, numericality: { other_than: 0, message: "Select" }
     validates :phone_number, format: { with: /\A\d{11}\z/ }
@@ -12,6 +13,6 @@ class PurchasedItemAddress
 
   def save
     PurchasedItem.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, address_detail: address_detail, building: building, phone_number: phone_number, purchased_item: purchased_item)
+    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, address_detail: address_detail, building: building, phone_number: phone_number, purchased_item_id: item_id)
   end
 end
